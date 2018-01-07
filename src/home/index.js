d4d524f94517f716e6874d07ee1b728afff8fd89
Hampton <hpbrew@gmail.com>
2018-01-06 21:48:45 -0600

    Added all gantt dependencies and a test gantt.

--- src/home/index.js
+++ src/home/index.js
@@ -1,2 +1,19 @@
 angular
-    .module('home', ['ui.router', 'gantt','gantt.table','gantt.movable', 'gantt.tooltips'])
+    .module('home', [
+        'ui.router',
+        'gantt',
+        'gantt.table',
+        'gantt.movable',
+        'gantt.tooltips',
+        'gantt.labels',
+        'gantt.corner',
+        'gantt.groups',
+        'gantt.bounds',
+        'gantt.sections',
+        'gantt.progress',
+        'gantt.resizeSensor',
+        'gantt.tree',
+        'gantt.dependencies',
+        'gantt.sortable',
+        // 'gantt.drawtask',
+    ])
